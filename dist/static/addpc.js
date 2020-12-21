9928d74c3bddbb7ad912eb03e54d173b70b89f9c
chamod-github <48453186+chamod-github@users.noreply.github.com>
2020-12-21 15:01:35 -0700

    addpc.js checks for required pc specs and submits a http request upon submit pc click.

--- dist/static/addpc.js
+++ dist/static/addpc.js
@@ -1,4 +1,5 @@
 "use strict";
+let id = 1;
 function main() {
     document
         .querySelector("#submit")
@@ -7,6 +8,9 @@ function main() {
         .querySelector("#reset")
         .addEventListener("click", onResetButtonClick);
     document.querySelector("#add-part").addEventListener("click", onAddSpecClick);
+    document
+        .querySelector("#add-image")
+        .addEventListener("click", onAddImageClick);
     document.addEventListener("keyup", function (event) {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -15,14 +19,93 @@ function main() {
     });
 }
 function onSubmitButtonClick() {
-    console.log("Submit button clicked.");
-    onSubmitCheckFields();
+    let pc = validateAndReturn();
+    if (pc != null) {
+        let client = new XMLHttpRequest();
+        client.onload = handler;
+        client.open("POST", window.location.origin + "/api/v1/pcs");
+        client.send(JSON.stringify(pc));
+    }
 }
-function onSubmitCheckFields() {
+function handler() {
+    if (this.status == 200) {
+        console.log(this.response);
+    }
+    else {
+        console.log("Request unsuccess.");
+    }
+}
+class Part {
+    constructor() {
+        this.type = "";
+        this.brand = "";
+        this.model = "";
+        this.qty = 1;
+    }
+}
+class PC {
+    constructor() {
+        this.name = "";
+        this.info = "";
+        this.parts = [];
+        this.images = [];
+    }
+    validateParts() {
+        for (const prop of [
+            "cpu",
+            "gpu",
+            "psu",
+            "case",
+            "disk",
+            "mobo",
+            "ram",
+            "cooler",
+        ]) {
+            const exist = this.parts.some((el) => el.type === prop);
+            if (!exist) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+class ImageLink {
+}
+function validateAndReturn() {
+    let spec = new PC();
     let form = document.querySelector("form");
+    if (!form.reportValidity()) {
+        return;
+    }
+    let name = form.querySelector("#name");
+    let info = form.querySelector("#info");
+    spec.name = name.value;
+    spec.info = info.value;
+    let parts = form.querySelectorAll(".part");
+    for (const part of parts) {
+        let partObj = new Part();
+        partObj.type = part.querySelector(".type").value;
+        partObj.brand = part.querySelector(".brand").value;
+        partObj.model = part.querySelector(".model").value;
+        partObj.qty = parseInt(part.querySelector(".qty").value);
+        spec.parts.push(partObj);
+    }
+    if (!spec.validateParts()) {
+        console.log("Missing required specs.");
+        return null;
+    }
+    let images = form.querySelectorAll(".image-link");
+    for (const image of images) {
+        let img = new ImageLink();
+        img.link = image.value;
+        spec.images.push(img);
+    }
+    return spec;
 }
 function onResetButtonClick() {
     console.log("Reset button clicked.");
+    let form = document.querySelector("form");
+    form.reset();
 }
 function onDeletePartClick(event) {
     let part = event.target.parentElement;
@@ -31,12 +114,27 @@ function onDeletePartClick(event) {
 function onAddSpecClick() {
     let templateInfo = document.querySelector("#pc-specs-templ").innerHTML;
     let template = Handlebars.compile(templateInfo);
-    let data = template({});
+    let data = template({ id: id++ });
     document.querySelector("#pc-specs").insertAdjacentHTML("beforeend", data);
     document
         .querySelectorAll(".part")
         .forEach((element) => element
         .querySelector("button")
         .addEventListener("click", onDeletePartClick));
+}
+function onDeleteImageClick(event) {
+    let image = event.target.parentElement;
+    image.remove();
+}
+function onAddImageClick() {
+    let templateInfo = document.querySelector("#pc-images-templ").innerHTML;
+    let template = Handlebars.compile(templateInfo);
+    let data = template({ id: id++ });
+    document.querySelector("#pc-images").insertAdjacentHTML("beforeend", data);
+    document
+        .querySelectorAll(".image")
+        .forEach((element) => element
+        .querySelector("button")
+        .addEventListener("click", onDeleteImageClick));
 }
 window.addEventListener("DOMContentLoaded", main);
--- src/addpc.ts
+++ src/addpc.ts
@@ -1,3 +1,6 @@
+// Global id coutner for ids for elements
+let id = 1;
+
 /**
  * main function handles when DOM loaded
  */
@@ -9,6 +12,10 @@ function main() {
     .querySelector("#reset")
     .addEventListener("click", onResetButtonClick);
   document.querySelector("#add-part").addEventListener("click", onAddSpecClick);
+  document
+    .querySelector("#add-image")
+    .addEventListener("click", onAddImageClick);
+  // Source: https://stackoverflow.com/a/63104461
   document.addEventListener("keyup", function (event) {
     // Number 13 is the "Enter" key on the keyboard
     if (event.key === "Enter") {
@@ -21,15 +28,135 @@ function main() {
 }
 
 /**
- * TODO
+ * TODO add comments
  */
 function onSubmitButtonClick() {
-  console.log("Submit button clicked.");
-  onSubmitCheckFields();
+  let pc = validateAndReturn();
+  if (pc != null) {
+    let client: XMLHttpRequest = new XMLHttpRequest();
+    client.onload = handler;
+    client.open("POST", window.location.origin + "/api/v1/pcs");
+    client.send(JSON.stringify(pc));
+  }
 }
 
-function onSubmitCheckFields() {
-  let form = document.querySelector("form");
+// Source: https://xhr.spec.whatwg.org/
+function handler(this: XMLHttpRequest) {
+  if (this.status == 200) {
+    // success!
+    console.log(this.response);
+
+  } else {
+    console.log("Request unsuccess.");
+  }
+}
+
+
+
+
+
+
+/**
+ * A part object containing part info
+ * verify method to check for empty values
+ */
+class Part {
+  type: string;
+  brand: string;
+  model: string;
+  qty: number;
+  constructor() {
+    this.type = "";
+    this.brand = "";
+    this.model = "";
+    this.qty = 1;
+  }
+}
+
+/**
+ * Spec object containing PC info
+ */
+class PC {
+  name: string;
+  info: string;
+  parts: Part[];
+  images: ImageLink[];
+  constructor() {
+    this.name = "";
+    this.info = "";
+    this.parts = [];
+    this.images = [];
+  }
+
+  /**
+   * Check if at least a required component is missing.
+   */
+  validateParts() {
+    for (const prop of [
+      "cpu",
+      "gpu",
+      "psu",
+      "case",
+      "disk",
+      "mobo",
+      "ram",
+      "cooler",
+    ]) {
+      const exist = this.parts.some((el) => el.type === prop);
+      if (!exist) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+class ImageLink {
+  link: string;
+}
+
+/**
+ * Check for validity and get the values
+ */
+function validateAndReturn() {
+  let spec = new PC();
+
+  let form: HTMLFormElement = document.querySelector("form");
+  if (!form.reportValidity()) {
+    return;
+  }
+  let name: HTMLTextAreaElement = form.querySelector("#name");
+  let info: HTMLTextAreaElement = form.querySelector("#info");
+  spec.name = name.value;
+  spec.info = info.value;
+
+  // Get all parts and put in the parts array
+  let parts = form.querySelectorAll(".part");
+  for (const part of parts) {
+    let partObj = new Part();
+    partObj.type = (part.querySelector(".type") as HTMLTextAreaElement).value;
+    partObj.brand = (part.querySelector(".brand") as HTMLTextAreaElement).value;
+    partObj.model = (part.querySelector(".model") as HTMLTextAreaElement).value;
+    partObj.qty = parseInt(
+      (part.querySelector(".qty") as HTMLTextAreaElement).value
+    );
+    spec.parts.push(partObj);
+  }
+
+  // Check for required components
+  if (!spec.validateParts()) {
+    console.log("Missing required specs.");
+    return null;
+  }
+
+  // Get all images
+  let images = form.querySelectorAll(".image-link");
+  for (const image of images) {
+    let img = new ImageLink();
+    img.link = (image as HTMLTextAreaElement).value;
+    spec.images.push(img);
+  }
+  return spec;
 }
 
 /**
@@ -37,6 +164,8 @@ function onSubmitCheckFields() {
  */
 function onResetButtonClick() {
   console.log("Reset button clicked.");
+  let form: HTMLFormElement = document.querySelector("form");
+  form.reset();
 }
 
 /**
@@ -44,8 +173,8 @@ function onResetButtonClick() {
  * @param event Event associated with the click
  */
 function onDeletePartClick(event: any) {
-  // get parent part
-  let part: HTMLElement = event.target.parentElement;
+  // get parent part of the remove button
+  let part: HTMLDivElement = event.target.parentElement;
   part.remove();
 }
 /**
@@ -55,7 +184,7 @@ function onAddSpecClick() {
   let templateInfo = document.querySelector("#pc-specs-templ").innerHTML;
   let template = Handlebars.compile(templateInfo);
 
-  let data = template({});
+  let data = template({ id: id++ });
 
   document.querySelector("#pc-specs").insertAdjacentHTML("beforeend", data);
 
@@ -65,6 +194,36 @@ function onAddSpecClick() {
       element
         .querySelector("button")
         .addEventListener("click", onDeletePartClick)
+    );
+}
+
+/**
+ * Remove an image given the remove button event
+ * @param event Event associated with the click
+ */
+function onDeleteImageClick(event: any) {
+  // get parent image of the remove button
+  let image: HTMLDivElement = event.target.parentElement;
+  image.remove();
+}
+
+/**
+ * Add image area upon add image button click
+ */
+function onAddImageClick() {
+  let templateInfo = document.querySelector("#pc-images-templ").innerHTML;
+  let template = Handlebars.compile(templateInfo);
+
+  let data = template({ id: id++ });
+
+  document.querySelector("#pc-images").insertAdjacentHTML("beforeend", data);
+
+  document
+    .querySelectorAll(".image")
+    .forEach((element) =>
+      element
+        .querySelector("button")
+        .addEventListener("click", onDeleteImageClick)
     );
 }
 
